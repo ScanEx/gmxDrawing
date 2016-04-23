c65907477ee2e172c4db8affcadddcbbe8c028a9
OriginalSin <saleks@scanex.ru>
2016-04-23 15:17:07 +0300

    detect IE Edge

--- src/L.GmxDrawing.Ring.js
+++ src/L.GmxDrawing.Ring.js
@@ -220,7 +220,7 @@ L.GmxDrawing.Ring = L.LayerGroup.extend({
 
     // edit mode
     _pointDown: function (ev) {
-        if (L.Browser.ie || L.gmxUtil.isIE12) {
+        if (L.Browser.ie || L.gmxUtil.gtIE11) {
             this._map.dragging._draggable._onUp(); // error in IE
         }
         this._parent._disableDrag();
