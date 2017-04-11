1d3d4075da30a29e024334d40fff5be7ef57ce90
OriginalSin <saleks@scanex.ru>
2017-04-11 16:39:31 +0300

    drawstop event for pointUp

--- src/L.GmxDrawing.Ring.js
+++ src/L.GmxDrawing.Ring.js
@@ -331,9 +331,9 @@ L.GmxDrawing.Ring = L.LayerGroup.extend({
             }
             this._map._skipClick = true;    // for EventsManager
         }
-        if (this._drawstop) {
+        // if (this._drawstop) {
             this._fireEvent('drawstop');
-        }
+        // }
         this._drawstop = false;
         this.down = null;
         var lineStyle = this.options.lineStyle || {};
